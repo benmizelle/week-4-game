a64d3956145f2e5741473ad3c34d5761aaf5a6aa
Ben Mizelle <Ben Mizelle>
2017-02-13 00:05:24 -0500

    hover in jquery

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+var nord = 0;
+var highElf = 0;
+var khajit = 0;
+var orc = 0;
+
+   $(".img-circle").hover(function() {
+    $(this).animate({'height':'200px', 'width':'200px'});
+
+// close hover function
+});
+// close document ready function
+});
