45df710342c680a5175c398633833e7c16ecd6eb
Ben Mizelle <Ben Mizelle>
2017-02-13 23:08:43 -0500

    moddified buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,66 @@
 
 $(document).ready(function(){
 
-var nord = 0;
-var highElf = 0;
-var khajit = 0;
-var orc = 0;
+var nord = null;
+var highElf = null;
+var khajit = null;
+var orc = null;
 
    $(".img-circle").hover(function() {
     $(this).animate({'height':'170px', 'width':'170px'});
-
 // close hover function
 });
- $(".img-circle").click(function(){
-    alert("image is clicked");
+
+ $("#nord").click(function(){
+    console.log("image is clicked");
+
+if (nord == null) {
+
+	nord = this.value
+    document.getElementById("nord").innerHTML = "";
+    }
+
 // close img-circle function
 });
 
+$("#high-elf").click(function(){
+    console.log("image is clicked");
 
 
+if (highElf == null) {
+
+	highElf = this.value
+    document.getElementById("high-elf").innerHTML = "";
+// close if statement
+    }
+// close img-circle function
+});
+
+$("#khajit").click(function(){
+    console.log("image is clicked");
 
 
+if (khajit == null) {
+
+	khajit = this.value
+    document.getElementById("khajit").innerHTML = "";
+// close if statement
+    }
+// close img-circle function
+});
+
+$("#orc").click(function(){
+    console.log("image is clicked");
 
 
+if (orc == null) {
 
+	orc = this.value
+    document.getElementById("orc").innerHTML = "";
+// close if statement
+    }
+// close img-circle function
+});
 
 
 
